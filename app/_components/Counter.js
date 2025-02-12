a062d920706e77ed029fe6a3bd4c37d9b9ccb683
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-02-12 19:53:36 +0100

    Adding title and favicon

--- app/_components/Counter.js
+++ app/_components/Counter.js
@@ -4,18 +4,11 @@ import { useState } from "react";
 
 export default function Counter({ data }) {
   const [count, setCount] = useState(0);
-  const [visible, setVisible] = useState(false);
   console.log(data);
 
   return (
     <>
       <p>There are {data.length} users</p>
-
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
 
       <button onClick={() => setCount((c) => c + 1)}>{count}</button>
     </>
