4aeeb73263c76bfb9dcd834f3bd22e6e5e16bfe4
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-02-12 20:22:20 +0100

    Making home page layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,8 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
-
 import { Josefin_Sans } from "next/font/google";
+import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
-
-import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -21,14 +18,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 antialiased text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
