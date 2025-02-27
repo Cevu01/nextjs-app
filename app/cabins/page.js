70e4248f74ab2f98489126b2401c5314f3d4582a
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-02-27 18:09:20 +0100

    Converting some components that are interactive or needs state to client components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 
+export const revalidate = 15; // ovde mora biti broj a ne varijabla
+
 export const metadata = {
   title: "Cabins",
 };
