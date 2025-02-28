b1d5ca9929fb11227d8258cd71f1dbee836c53eb
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-02-28 09:44:13 +0100

    Refaktorisanje koda

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 15; // ovde mora biti broj a ne varijabla
+//zbog searchParams ovo je sada dinamicki renderovano, tako da revalidate ne mora biti ovde hahaha
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,9 +26,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
