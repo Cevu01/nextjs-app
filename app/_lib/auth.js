b98ae3fc66cc593eb54e8812d25791ac258dbc3f
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-03-03 01:16:56 +0100

    Connecting users to supabase

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,7 +13,26 @@ const authConfig = {
     authorized({ auth, request }) {
       return !!auth?.user; // ako ima usera onda je ovo true, ako ne onda je false, pa zbog false ne mozemo da pristupimo /account iz middleware.js
     },
+    //ovaj signIn callback izvrsava se pre nego sto se signUp uopste desi
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
   },
+
   //ovako navigiramo korisnika na /login stranicu kada on pokusa da klikne protected rutu
   pages: {
     signIn: "/login",
